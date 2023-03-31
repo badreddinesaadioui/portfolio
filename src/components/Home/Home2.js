e9240f02cdedf1371800c197d5f690ec1e263465
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-03-31 08:27:05 +0100

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,44 +19,32 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              Hi there! I'm a system engineering student at Ecole centrale casablanca,
-              and I've been captivated by the world of programming since I first started. 
-              While I'm still learning, I'm proud to say that I'm quite comfortable with data analysis and AI engineering.
-
-             My interests lie in developing cutting-edge web technologies and products, as well as exploring the fascinating 
-             fields of deep learning and computer vision. I'm constantly seeking out new challenges and opportunities to apply 
-              my skills in Python and JAVA to build innovative products that can make a real impact in the world. Thanks for taking 
-              the time to get to know me a little better!
-                
-              Hi there! I'm a system engineering student at Ecole centrale casablanca,
-              and I've been captivated by the world of programming since I first started...🤷‍♂️
+              I fell in love with programming and I have at least learnt
+              something, I think… 🤷‍♂️
               <br />
               <br />I am fluent in classics like
-                <b className="purple"> Javascript and Python. </b>
+              <i>
+                <b className="purple"> JAVA, C, Javascript and Python. </b>
               </i>
               <br />
               <br />
-              My field of Interest's are &nbsp;
+              My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Data analysis </b> and
-                {" "}
+                <b className="purple">Web Technologies and Products </b> and
+                also in areas related to{" "}
                 <b className="purple">
-                  AI engineering
+                  Deep Learning and Computer Vision.
                 </b>
               </i>
               <br />
               <br />
               Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              with <b className="purple">Python</b> and
               <i>
                 <b className="purple">
                   {" "}
-                  Modern Javascript Library and Frameworks
+                  JAVA.
                 </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
               </i>
             </p>
           </Col>
@@ -75,7 +63,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/Abdelmouhaimen"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -85,7 +73,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
+                  href="https://twitter.com/KyNgiaN"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -95,7 +83,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/Abdelmouhaimen"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -105,7 +93,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/KyNgiaN"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
