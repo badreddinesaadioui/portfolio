57d8f5513197ea8916869a552968fdcc27780433
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-04-03 15:23:06 +0100

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -61,7 +61,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/Abdelmouhaimen"
+                  href="https://github.com/badreddinesaadioui"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
