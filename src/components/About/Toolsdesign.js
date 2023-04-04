96a402621cdb64b607e961bd07f068f690ac96bc
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-04-04 10:17:47 +0100

    Create Toolsdesign.js

---
+++ src/components/About/Toolsdesign.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiAdobeaftereffects,
+  SiAdobeillustrator,
+  SiAdobephotoshop,
+  SiAdobeindesign,
+  SiCanva,
+  SiBlender,
+  
+} from "react-icons/si";
+
+
+function Toolsdesign() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAdobeaftereffects />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAdobeillustrator />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAdobephotoshop />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAdobeindesign />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiBlender />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiCanva />
+        </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
