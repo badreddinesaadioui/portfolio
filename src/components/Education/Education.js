d52a7d591daee574eeb3f1b71e98b89986a1ae29
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-04-04 12:32:29 +0100

    Update Education.js

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -33,10 +33,10 @@ function Education() {
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
     icon={<IoMdSchool />}
   >
-    <h3 className="vertical-timeline-element-title">ECC CENTRALE CASABLANCA | system engineering Student</h3>
+    <h3 className="vertical-timeline-element-title"> CPGE LYCEE MED V | MPSI-MP </h3>
     <h4 className="vertical-timeline-element-subtitle">Casablanca, Morocco</h4>
     <p>
-      I joined The ÉCOLE CENTRALE CASABLANCA ECC which is a French/Morroccan engineering school (Grande École) that provides a broad-based education encompassing a wide range of subjects.
+      A 2 year formation balanced between mathematics, physical sciences, computer science and industrial sciences, which ideally prepares for future engineering professions.
       
     </p>
   </VerticalTimelineElement>
