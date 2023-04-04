9f15d4bf3f1fe41d9a5f6f12cd94652119fae77f
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-04-04 23:07:42 +0100

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import postgresql from "../../Assets/Projects/PostegreSQL.png";
-import tipe from "../../Assets/Projects/tipe.png";
-import myjobglasses from "../../Assets/Projects/myjobglasses.png";
+import cancer from "../../Assets/Projects/cancer.png";
+import genes from "../../Assets/Projects/genes.png";
+import traffic from "../../Assets/Projects/traffic.png";
 
 
 function Projects() {
@@ -21,7 +21,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={postgresql}
+              imgPath={cancer}
               isBlog={false}
               title="Detection Of Microcalcifications In Mammography"
               description="In this project, we conducted a comprehensive analysis of various machine learning models, including KNN, SVM, and random forest, with different parameter settings and combinations, to accurately detect microcalcifications on mammograms. Early detection of microcalcifications is crucial in breast cancer screening. However, the dataset used in this project was highly imbalanced, which presented a significant challenge. This project was supervised by ECC CODING WEEK."
@@ -32,7 +32,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={tipe}
+              imgPath={genes}
               isBlog={false}
               title="TIPE Genetic Disease Propagation: A Population Modeling and Analysis Approach."
               description="Supervised personal initiative project or TIPE is a common test to most entrance examinations to the Grandes Ecoles of Science. Throughout my project, I explored the relationship between genetics, statistics, and the spread of diseases. I began by delving into the importance of probabilities in genetics and how they are vital for genetic studies. From there, I went on to write a Python code to model the spread of a disease over time. Using this code, I was able to investigate the statistical evolution of a general hereditary disease, and gain insight into how these diseases'frequency vary over time."
@@ -42,7 +42,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={myjobglasses}
+              imgPath={traffic}
               isBlog={false}
               title="PLBD : Traffic Optimization"
               description="The Project Learning by Doing is an annual project that encourages teamwork and leadership development among a team of five members with diverse nationalities. In this project, we utilized OpenCV to analyze traffic images and extract traffic density information, which enabled us to develop an optimization code that dynamically adjusted traffic light timings to improve traffic flow and alleviate congestion in real-time. To assess the effectiveness of our optimization code, we compared our model with both a deep learning-based approach and a traditional timing system using a traffic simulator. Our results highlighted the potential of computer vision and machine learning techniques to optimize traffic control systems."           
