d1eef8c53ba4477bf8e1a68db51e68fded1eab0d
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-04-04 03:23:56 +0100

    Update Github.js

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -9,7 +9,7 @@ function Github() {
         Days I <strong className="purple">Code</strong>
       </h1>
       <GitHubCalendar
-        username="Abdelmouhaimen"
+        username="Badreddine"
         blockSize={15}
         blockMargin={5}
         color="#3161c2"
