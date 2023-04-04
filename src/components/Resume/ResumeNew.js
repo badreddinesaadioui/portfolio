637a259ada5625d782e2cbca5d41f132ef247655
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-04-04 11:26:37 +0100

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -4,12 +4,12 @@ import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/CV_BADREDDINESAADIOUI_ECC.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
+import { pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "https://sarhane.com/static/media/CV_BADREDDINESAADIOUI_ECC.6035337128a141c1540f.pdf";
+const resumeLink = "https://sarhane.com/static/media/CV_BADREDDINESAADIOUI_ECC.6035337128a141c1540f.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -33,27 +33,10 @@ function ResumeNew() {
             &nbsp;Download CV
           </Button>
         </Row>
-
-        <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
       </Container>
     </div>
   );
 }
 
 export default ResumeNew;
+
