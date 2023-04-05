fda756dd38aed761eda334309b5d5bc359731097
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-04-05 06:14:18 +0100

    Update International.js

--- src/components/International/International.js
+++ src/components/International/International.js
@@ -11,18 +11,26 @@ function International() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          International <strong className="purple"> Mobility </strong>
+          Design <strong className="purple"> Skills </strong>
         </h1>
         <p style={{ color: "white" }}>
-        As a Moroccan student living and studying in France, I had the chance to discover different cultures and get to know and work with international students too, which is necessary in the field of computer science to work with people from different parts of the globe. That's why in addition to English, French and Arabic; I decided to choose Japanese language as my LV2 to strengthen my relations with new people
+        Design is an increasingly crucial and in-demand aspect of various industries as the digital world advances. Fortunately, my lifelong passion for art has led me to hone my skills and specialize in this field, which I consider a natural extension of my abilities. My background in art has given me a profound understanding of design principles and techniques, allowing me to create visually stunning and innovative designs. I find the creative aspect of design work to be immensely fulfilling and enjoyable, and it is a true passion of mine. Continuously seeking new ways to push the boundaries of what is possible in this dynamic and exciting field, I am dedicated to delivering exceptional design solutions to clients.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={travel}
               isBlog={false}
-              title=" Travel the world"
+              title="Travel the world"
               description="I have the intent to visit all countries of the world and discover new things, and to be able to express with others differently. I haven't traveled yet to many countries, I have been in Morocco, France, Spain and Portugal, I really liked those experiences."           
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={travel}
+              isBlog={false}
+              title="Another Project"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."           
             />
           </Col>
         </Row>
