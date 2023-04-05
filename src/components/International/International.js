3c533aa2fee729f7c4b05a89025d46d7f48d0902
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-04-05 14:21:34 +0100

    Update International.js

--- src/components/International/International.js
+++ src/components/International/International.js
@@ -16,7 +16,9 @@ function International() {
         <p style={{ color: "white" }}>
         Design is an increasingly crucial and in-demand aspect of various industries as the digital world advances. Fortunately, my lifelong passion for art has led me to hone my skills and specialize in this field, which I consider a natural extension of my abilities. My background in art has given me a profound understanding of design principles and techniques, allowing me to create visually stunning and innovative designs. I find the creative aspect of design work to be immensely fulfilling and enjoyable, and it is a true passion of mine. Continuously seeking new ways to push the boundaries of what is possible in this dynamic and exciting field, I am dedicated to delivering exceptional design solutions to clients.
         </p>
-        <h2 className="project-subheading">My Projects</h2>
+        <h1 className="project-heading">
+          What i can <strong className="purple"> Do </strong>
+        </h1>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
