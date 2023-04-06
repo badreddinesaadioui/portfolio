c0a840874e41b16f4dc87b6d529dfa83db4fbcb6
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-04-07 00:40:07 +0100

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,7 +27,7 @@ function Projects() {
               description="In this project, we conducted a comprehensive analysis of various machine learning models, including KNN, SVM, and random forest, with different parameter settings and combinations, to accurately detect microcalcifications on mammograms. Early detection of microcalcifications is crucial in breast cancer screening. However, the dataset used in this project was highly imbalanced, which presented a significant challenge. This project was supervised by ECC CODING WEEK."
               demoLink="https://youtu.be/hXpkJLSBqd0"
               ghLink="https://github.com/badreddinesaadioui/Detection-Of-Microcalcifications-In-Mammography"
-            />
+              />
           </Col>
 
           <Col md={4} className="project-card">
@@ -37,7 +37,7 @@ function Projects() {
               title="TIPE Genetic Disease Propagation: A Population Modeling and Analysis Approach."
               description="Supervised personal initiative project or TIPE is a common test to most entrance examinations to the Grandes Ecoles of Science. Throughout my project, I explored the relationship between genetics, statistics, and the spread of diseases. I began by delving into the importance of probabilities in genetics and how they are vital for genetic studies. From there, I went on to write a Python code to model the spread of a disease over time. Using this code, I was able to investigate the statistical evolution of a general hereditary disease, and gain insight into how these diseases'frequency vary over time."
               ghLink="https://github.com/badreddinesaadioui/TIPE"
-            />
+              />
           </Col>
 
           <Col md={4} className="project-card">
@@ -46,7 +46,7 @@ function Projects() {
               isBlog={false}
               title="PLBD : Traffic Optimization"
               description="The Project Learning by Doing is an annual project that encourages teamwork and leadership development among a team of five members with diverse nationalities. In this project, we utilized OpenCV to analyze traffic images and extract traffic density information, which enabled us to develop an optimization code that dynamically adjusted traffic light timings to improve traffic flow and alleviate congestion in real-time. To assess the effectiveness of our optimization code, we compared our model with both a deep learning-based approach and a traditional timing system using a traffic simulator. Our results highlighted the potential of computer vision and machine learning techniques to optimize traffic control systems."           
-            />
+              />
           </Col>
 
         </Row>
