3dea973af1cbe250938b3c5a63aafe08818b72d2
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-04-07 12:01:59 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ import { IoMdSchool } from "react-icons/io";
 
 
 import { MdOutlineDesignServices } from "react-icons/md";
-import {GrCertificate} from "react-icons/gr";
+import {TbCertificate} from "react-icons/tb";
 import { CgGames } from 'react-icons/cg';
 
 
@@ -127,7 +127,7 @@ function NavBar() {
                 to="/civic"
                 onClick={() => updateExpanded(false)}
               >
-                <GrCertificate color='white' style={{ marginBottom: "2px" }} /> Certification
+                <TbCertificate style={{ marginBottom: "2px" }} /> Certification
               </Nav.Link>
             </Nav.Item>
 
