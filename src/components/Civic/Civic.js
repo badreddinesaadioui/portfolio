1b2677d87a3ff0f3bc8e2038768a1810bf9b2bf1
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-04-07 15:33:57 +0100

    Update Civic.js

--- src/components/Civic/Civic.js
+++ src/components/Civic/Civic.js
@@ -11,10 +11,10 @@ function Civic() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          Environmental <strong className="purple"> Projects </strong>
+          Online <strong className="purple"> Certifications </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Green actions are happening in this section ...
+          in this section you'll find certifications of courses that i enrolled...
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
@@ -23,6 +23,7 @@ function Civic() {
               isBlog={false}
               title="Google Project Management"
               description=" Google Project Management on Coursera is a comprehensive online course that provides learners with the knowledge and skills to effectively manage projects using Google's tools and methodologies. The course covers topics such as project planning, execution, monitoring, and closure, as well as effective communication, team collaboration, and risk management. Learners will also gain insights into Google's project management philosophy and best practices. The course is self-paced and offers a mix of instructional videos, interactive assignments, and real-world case studies to help learners develop practical project management skills using Google's approach."
+              courseraLink="https://www.coursera.org/account/accomplishments/professional-cert/ABYJPG7EJGQC?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=prof"
           />
           </Col>    
           
