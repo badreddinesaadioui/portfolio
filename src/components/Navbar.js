4ea0a4336f57d6f2282a9ab554c7e59e108ed558
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-04-08 20:22:30 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -114,7 +114,7 @@ function NavBar() {
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/international"
+                to="/design"
                 onClick={() => updateExpanded(false)}
               >
                 <MdOutlineDesignServices style={{ marginBottom: "2px" }} /> Design
@@ -124,7 +124,7 @@ function NavBar() {
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/civic"
+                to="/certification"
                 onClick={() => updateExpanded(false)}
               >
                 <FaCertificate style={{ marginBottom: "2px" }} /> Certification
