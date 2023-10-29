4804e53ef380dff2ab0dc7c0ab4cf965ed8ed023
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2023-10-29 05:18:11 +0100

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/CV_BADREDDINESAADIOUI_ECC.pdf";
+import pdf from "../../Assets/../Assets/CV _ SAADIOUI Badreddine.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
