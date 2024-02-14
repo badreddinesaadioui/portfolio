7f419c57473839946841e731e886b3d5ca8657e9
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2024-02-14 08:27:34 +0100

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,14 +9,14 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Badreddine Saadioui </span>
-            <br />I am first year system engineering student at ECC CENTRALE CASABLANCA. Interested in Artificial Intelligence and data science because of the mixture of applied mathematics, modelization, data analysis and software engineering. My curiosity towards science and new technologies is growing and supports my engagement to broaden my knowledge and build my skills.
+            <br />Second year generalist engineering ex-change student at ECL CENTRALE de Lyon. Interested in Artificial Intelligence and data science and consulting because of the mixture of applied mathematics, modelization, data analysis and software engineering. My curiosity towards science, new technologies and entrepreneurship is growing and supports my engagement to broaden my knowledge and build my skills.
 
 
             <br />
             <br />This portfolio seeks to best convey my character, my professional trajectory, and my future goals. You can find information about my experiences, initiatives, and skills, both professionally and personally. Please get in touch with me if you want additional information about my experience or the projects I worked on.
             <br />
             <br />
-            <br />Apart from coding, some other activities that I love to do!
+            <br />Apart from consulting and data, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
@@ -26,12 +26,12 @@ function AboutCard() {
               <ImPointRight /> Bodybuilding
             </li>
             <li className="about-activity">
-              <ImPointRight /> Sports
+              <ImPointRight /> Sports ( All of them XD )
             </li>
           </ul>
 
           <p style={{ color: "rgb(96, 136, 224)" }}>
-            "Empower your imagination to make the impossible inevitable!"{" "}
+            "Ta7aja matghlebna, rjal brjal wrjal blah !"{" "}
           </p>
           <footer className="blockquote-footer"> Badreddine Saadioui</footer>
         </blockquote>
