cba8373ba5c6fee444e19a0cc6047d69b44555bd
badreddinesaadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2024-02-15 17:30:41 +0100

    Update Experience.js

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -14,14 +14,14 @@ function Experience() {
     className="vertical-timeline-element--work"
     contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
     contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-    date="2023 - present"
+    date="June 2023 - July 2023"
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
     icon={<IoMdSchool />}
   >
-    <h3 className="vertical-timeline-element-title">ECL ECOLE CENTRALE DE LYON | Ex-change generalist engineering Student</h3>
-    <h4 className="vertical-timeline-element-subtitle">Lyon, France</h4>
+    <h3 className="vertical-timeline-element-title">CFG BANK | AI and Software Engineering Intern</h3>
+    <h4 className="vertical-timeline-element-subtitle">Casablanca, Morocco</h4>
     <p>
-      Ex-change engineering student at ÉCOLE CENTRALE de Lyon (Grande École) studying Computer science, finance and economics, marketing, entrepreneurship and astrophysics.
+      A 6-week internship, where i designed and developed an API for quality prospect leads gathering from Creditinfo, integrated it with the Pipedrive CRM used by the marketing team to alert them given also necessary infos about the prospect that i extract, also had a second mission of developping a client assistant chatbot, used dialogflow and extracted data from different sources such as CFG Bank website and dozens of documents.
 
     </p>
   </VerticalTimelineElement>
@@ -29,14 +29,28 @@ function Experience() {
     className="vertical-timeline-element--work"
     contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
     contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-    date="2022 - 2023"
+    date="February 2023 - Present"
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
     icon={<IoMdSchool />}
   >
-    <h3 className="vertical-timeline-element-title">ECC CENTRALE CASABLANCA | system engineering Student</h3>
+    <h3 className="vertical-timeline-element-title">JECC Junior Entreprise | Junior Consultant</h3>
     <h4 className="vertical-timeline-element-subtitle">Casablanca, Morocco</h4>
     <p>
-      I joined The ÉCOLE CENTRALE CASABLANCA ECC which is a French/Morroccan engineering school (Grande École) that provides a broad-based education encompassing a wide range of subjects.
+      held two roles: Junior consultant, where I managed and spearheaded projects such as TRIA Company, overseeing comprehensive market and competitor analyses through conducting a 600 targeted surveys across various locations in Morocco (including Rabat, Casablanca, Tangier, Agadir, Tetouan, etc.), and utilizing Power BI to create informative dashboards aiding strategic decision-making; subsequently, I served as a graphic designer.    
+    </p>
+  </VerticalTimelineElement>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--work"
+    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+    date="March 2023 - Present "
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+    icon={<IoMdSchool />}
+  >
+    <h3 className="vertical-timeline-element-title"> GrowStudio Agency | Founder </h3>
+    <h4 className="vertical-timeline-element-subtitle">Casablanca, Morocco</h4>
+    <p>
+      Having expertise in design, I found myself drawn to marketing and design, which led me to establish GrowStudio, a digital marketing agency. Here, I've honed my skills in Web Development, Branding, SEO, Google Maps profile optimization, Logo design, and Strategic thinking. I've helped businesses attract more clients, particularly through Google Maps, by enhancing their online presence. You can explore my business at <a href='https://grow.studio' target='_blank'>https://grow.studio</a>. Some of our clients include MTSAUTO, showcased at <a href='https://MTSauto.ma' target='_blank'>https://MTSauto.ma</a>.
       
     </p>
   </VerticalTimelineElement>
@@ -44,30 +58,14 @@ function Experience() {
     className="vertical-timeline-element--work"
     contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
     contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-    date="2020 - 2022"
+    date="2022 - Present"
     iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
     icon={<IoMdSchool />}
   >
-    <h3 className="vertical-timeline-element-title"> CPGE LYCEE MED V | MPSI-MP </h3>
+    <h3 className="vertical-timeline-element-title"> DRIPIO clothing| Founder </h3>
     <h4 className="vertical-timeline-element-subtitle">Casablanca, Morocco</h4>
     <p>
-      A 2 year formation balanced between mathematics, physical sciences, computer science and industrial sciences, which ideally prepares for future engineering professions.
-      
-    </p>
-  </VerticalTimelineElement>
-  <VerticalTimelineElement
-    className="vertical-timeline-element--work"
-    contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-    date="2019 - 2020"
-    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-    icon={<IoMdSchool />}
-  >
-    <h3 className="vertical-timeline-element-title"> LYCEE AZHAR ERRIAD 2 | High School Diploma </h3>
-    <h4 className="vertical-timeline-element-subtitle">Casablanca, Morocco</h4>
-    <p>
-      Option : Science mathématiques B (Mention très bien)
-    </p>
+      Falling also within my design expertise, I founded Dripio, an online clothing store showcasing my original creations. At Dripio, I collaborate closely with textile production suppliers for Jackets productions and utilize direct-to-fabric (DTF) printing or embroidery techniques. From design conception to production, I oversee every step to ensure quality and consistency. Visit my store at <a href='https://dripio.youcan.store/' target='_blank'>https://dripio.youcan.store/</a>.
   </VerticalTimelineElement>
 </VerticalTimeline>
   );
