b188695499e2dcd1181ad174d513a098fa978f43
Badreddine Saadioui <129481192+badreddinesaadioui@users.noreply.github.com>
2024-11-01 01:32:39 +0100

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
+  DiJavascript1,
+  DiReact,
   DiPython,
+  DiDjango,
+  DiDocker,
+  DiMysql,
+  DiGit,
+  DiHtml5,
+  DiCss3,
 } from "react-icons/di";
 import {
   SiOpencv,
@@ -10,10 +18,7 @@ import {
   SiPandas,
   SiPytorch,
   SiScikitlearn,
-  SiLangchain,
-  SiOpenai,
-  SiHuggingface,
-  SiDocker,
+  SiNumpy,
 } from "react-icons/si";
 
 function Techstack() {
@@ -23,34 +28,49 @@ function Techstack() {
         <DiPython />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiLangchain />
+        <SiPytorch />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiOpenai />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiHuggingface />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiDocker />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiOpencv />
+        <SiScikitlearn />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiTensorflow />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiKeras />
+        <SiNumpy />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiPytorch />
+        <SiKeras />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPandas />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiScikitlearn />
+        <SiOpencv />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMysql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiDjango />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiDocker />
       </Col>
     </Row>
   );
